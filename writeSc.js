e73a590bfb34eb809ea6ac0dde74302049ff6259
Vishal kumar <vishal.oneto11@gmail.com>
2022-11-21 11:21:42 +0530

    completed the read write with ether.js a

--- writeSc.js
+++ writeSc.js
@@ -7,6 +7,7 @@ const Privatekey = 'e85f531eadbc3f1b1d915bd69e4b47393f71d0750492cd79cdb642695af7
 const RPC = 'https://goerli.infura.io/v3/a4653f9706fc46f78d5c82d14687f3f1';
 const contractAddress = '0x6f5DF070E6f94D23AF2dE80047632995E7AE98Fd';
 const ABI = [{"inputs":[],"name":"getValue","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_val","type":"uint256"}],"name":"setValue","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]
+
 const provider = new ethers.providers.JsonRpcProvider(
     RPC
 )
